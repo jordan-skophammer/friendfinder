d92aa4d6793ca9b22fcd023075eedd0aa3019d54
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-12 18:31:42 -0500

    html routing works

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
   
-    app.get("/servey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/servey.html"));
+    app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
   
     app.get("*", function(req, res) {
