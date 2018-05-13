b72bd29fff96106f035dabd90072320213cf8a61
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-13 13:16:29 -0500

    compared scores and orders api by difference

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,18 +3,18 @@ var friendsArray = [
       name: "Jordan",
       photo: "https://cdn.nba.net/nba-drupal-prod/styles/landscape/http/i2.cdn.turner.com/nba/nba/dam/assets/130828123801-michael-jordan-iso-1998-all-star-game.video-player.jpg?itok=FGvQfRCL",
       scores: [
-        1,
-        2,
-        3,
-        4,
         5,
-        6,
-        7,
-        8,
-        9,
-        10
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
       ],
-      totalScore: 55
+      totalScore: 50
     }
   ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,13 +8,9 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     
-    friends.push(req.body);
-    res.json(true);
-
     var surveyScoreParse = req.body.scores.map(function(element) {
       
       return parseInt(element, 10)
-      
       
     })
     
@@ -25,7 +21,23 @@ module.exports = function(app) {
     var surveySum = surveyScoreParse.reduce(add, 0)
 
     req.body.totalScore = surveySum;
+    
+    for (let i = 0; i< friends.length; i++) {
+      var compareFriends = Math.abs(surveySum - friends[i].totalScore)
+
+      friends[i].compareScore = compareFriends
+
+      friends.sort(function(a, b) {
+        return a.compareScore - b.compareScore
+      })
+    }
+    
     console.log(friends)
+
+    friends.push(req.body);
+    res.json(true);
+
+    // console.log(friends)
   });
 
 };
