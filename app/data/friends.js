9011ee031f76f53ac22ffe7f43690703419574af
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-13 10:20:43 -0500

    survey score done

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,16 +2,18 @@ var friendsArray = [
     {
       name: "Jordan",
       photo: "https://cdn.nba.net/nba-drupal-prod/styles/landscape/http/i2.cdn.turner.com/nba/nba/dam/assets/130828123801-michael-jordan-iso-1998-all-star-game.video-player.jpg?itok=FGvQfRCL",
-      q1: 1,
-      q2: 2,
-      q3: 3,
-      q4: 4,
-      q5: 5,
-      q6: 6,
-      q7: 7,
-      q8: 8,
-      q9: 9,
-      q10: 10
+      scores: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10
+      ]
     }
   ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,23 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     
-      friends.push(req.body);
-      res.json(true);
+    friends.push(req.body);
+    res.json(true);
+
+    var surveyScoreParse = req.body.scores.map(function(element) {
+      
+      return parseInt(element, 10)
+      
+      
+    })
     
+    function add(a, b) {
+      return a + b
+    }
+
+    var surveySum = surveyScoreParse.reduce(add, 0)
+
+    console.log(surveySum)
   });
 
 };
