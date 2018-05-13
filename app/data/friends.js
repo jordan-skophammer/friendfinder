660ae1ac0fecd96db91f0b0acbec36e1fa8a838f
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-12 20:15:56 -0500

    survey object posting to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,7 @@
 var friendsArray = [
     {
+      name: "Jordan",
+      photo: "https://cdn.nba.net/nba-drupal-prod/styles/landscape/http/i2.cdn.turner.com/nba/nba/dam/assets/130828123801-michael-jordan-iso-1998-all-star-game.video-player.jpg?itok=FGvQfRCL",
       q1: 1,
       q2: 2,
       q3: 3,
