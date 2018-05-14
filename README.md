f89038119fec869edd95c136cc220dbd511e827b
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-14 05:53:37 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Friend Finder Survey
+
+This is app helps you connect to like minded friends. Once the survey is completed, the app will show
+you a friend with a score closest to yours. 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,37 +7,41 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    
+
+    //Parses the survey answers to intergers.
     var surveyScoreParse = req.body.scores.map(function(element) {
       
-      return parseInt(element, 10)
+      return parseInt(element, 10);
       
     })
     
+    //Adds the survey answers together.
     function add(a, b) {
       return a + b
-    }
+    };
 
-    var surveySum = surveyScoreParse.reduce(add, 0)
+    var surveySum = surveyScoreParse.reduce(add, 0);
 
+    //Appends the survey total to the user array.
     req.body.totalScore = surveySum;
-    // 
-    for (let i = 0; i< friends.length; i++) {
-      var compareFriends = Math.abs(surveySum - friends[i].totalScore)
 
-      friends[i].compareScore = compareFriends
+    //Compares the difference between the users score and the existing friends in the array.
+    //Then appends the score differnce and sorts the array my desending order.
+    for (let i = 0; i< friends.length; i++) {
+
+      var compareFriends = Math.abs(surveySum - friends[i].totalScore);
+
+      friends[i].compareScore = compareFriends;
 
       friends.sort(function(a, b) {
-        return a.compareScore - b.compareScore
+        return a.compareScore - b.compareScore;
       })
-    }
-    
-    // console.log(friends)
+    };
 
+    //Appends the users object to the friends array.
     friends.push(req.body);
     res.json(true);
 
-    // console.log(friends)
   });
 
 };
